642619fb11a639eca9d06b46403217838a2df75a
GabeG888 <thegabegordon@gmail.com>
2022-07-23 12:16:33 -0500

    Added an initial react frontend
    Similar to this tutorial: https://docs.mapbox.com/help/tutorials/use-mapbox-gl-js-with-react/.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FiZWc4ODgiLCJhIjoiY2w1dHdlMWNiMGRkYzNqczJ1NWxpd29nZSJ9.SqDMq9cSmJk8UFI2_oNnBw';
+ 
+export default function App() {
+const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-70.9);
+const [lat, setLat] = useState(42.35);
+const [zoom, setZoom] = useState(9);
+ 
+useEffect(() => {
+if (map.current) return; // initialize map only once
+map.current = new mapboxgl.Map({
+container: mapContainer.current,
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [lng, lat],
+zoom: zoom
+});
+});
+ 
+useEffect(() => {
+if (!map.current) return; // wait for map to initialize
+map.current.on('move', () => {
+setLng(map.current.getCenter().lng.toFixed(4));
+setLat(map.current.getCenter().lat.toFixed(4));
+setZoom(map.current.getZoom().toFixed(2));
+});
+});
+ 
+return (
+<div>
+<div className="sidebar">
+Spiritual Topography Map
+</div>
+<div ref={mapContainer} className="map-container" />
+</div>
+);
 }
-
-export default App;
