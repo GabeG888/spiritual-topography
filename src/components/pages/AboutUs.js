b6d603da556cfa1503e18d1c790a1bcc6f868e00
Frank Liu <frank.j.liu@berkeley.edu>
2022-08-03 16:59:27 -0400

    Updated AboutUs page

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,10 +1,59 @@
 import React from "react";
-import '../../App.css';
+import "./AboutUs.css";
 
 export default function AboutUs() {
   return (
-    <>
-      <h1 className="about-us">About Us</h1>
-    </>
+    <div className="wrapper">
+      <Card
+        img="./images/matt.jpg"
+        title="Matthew Chan"
+        role="Mentor"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+      <Card
+        img="./images/matt.jpg"
+        title="Alex Shih"
+        role="Mentor"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+      <Card
+        img="./images/gabbie.jpg"
+        title="Gabbie Liu"
+        role="Mentor"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+      <Card
+        img="./images/gabe.jpg"
+        title="Gabe Gordon"
+        role="Mentor"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+      <Card
+        img="./images/grace.jpg"
+        title="Grace Chan"
+        role="Mentor"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+      <Card
+        img="./images/frank.jpg"
+        title="Frank Liu"
+        role="Mentor"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      />
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src={props.img} class="card__image" />
+        <h2 className="card__title">{props.title}</h2>
+        <p className="card__role">{props.role}</p>
+        <p className="card__description">{props.description}</p>
+        <button className="card__btn">Connect</button>
+      </div>
+    </div>
   );
 }
