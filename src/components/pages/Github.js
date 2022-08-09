04c27037b021d9e2fbfcaa15cd41cbc30e120709
Frank Liu <frank.j.liu@berkeley.edu>
2022-08-09 12:38:02 -0400

    Linked github and modified Sidebar

--- src/components/pages/Github.js
+++ src/components/pages/Github.js
@@ -6,11 +6,17 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Github() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <FontAwesomeIcon
-          icon={faGithub}
-          className="animate-bounce text-gray text-9xl shadow-xl hover:shadow-2xl rounded-full font-bold transition ease-in-outbg-white hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
-        />
+      <div className="flex justify-center items-center h-screen shadow-xl hover:shadow-2xl hover:shadow-cyan-300">
+        <a
+          href="https://github.com/GabeG888/spiritual-topography.git"
+          target="_blank"
+          className="rounded-lg"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="p-8 animate-bounce text-gray text-9xl shadow-xl hover:shadow-2xl rounded-full font-bold transition ease-in-outbg-white hover:-translate-y-1 hover:scale-110 hover:bg-cyan-300 duration-300"
+          />
+        </a>
       </div>
       {/* <iframe
         src="../../README.md"
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
     <>
       {showSidebar ? (
         <button
-          className="flex text-4xl text-black items-center cursor-pointer fixed right-0 top-28 m-5 z-50"
+          className="p-4 flex text-4xl text-black items-center cursor-pointer fixed right-0 top-28 m-5 z-50"
           onClick={() => setShowSidebar(!showSidebar)}
         >
           <FontAwesomeIcon icon={faAnglesRight} className="text-black" />
@@ -17,18 +17,18 @@ const Sidebar = () => {
       ) : (
         <button
           onClick={() => setShowSidebar(!showSidebar)}
-          className="fixed z-30 flex items-center cursor-pointer right-0 top-28 m-5 text-black text-4xl"
+          className="bg-white p-4 rounded-full shadow-2xl shadow-white fixed z-30 flex items-center cursor-pointer right-0 top-28 m-5 text-black text-4xl"
         >
-          <FontAwesomeIcon icon={faAnglesLeft} className="text-white" />
+          <FontAwesomeIcon icon={faAnglesLeft} className="text-black" />
         </button>
       )}
 
       <div
-        className={`top-28 right-0 w-auto bg-white opacity-50 p-10 text-black fixed h-auto z-40 ease-in-out duration-300 ${
+        className={`top-28 right-0 w-auto bg-white bg-opacity-80 p-12 text-black fixed h-auto z-40 ease-in-out duration-300 rounded-lg shadow-2xl ${
           showSidebar ? "translate-x-0 " : "translate-x-full"
         }`}
       >
-        <h3 className="text-4xl font-semibold text-black">Options</h3>
+        <h3 className="text-4xl font-bold text-black">Options</h3>
         <br />
         <p className="text-xl text-black">
           <label className="relative flex justify-between items-center group p-2 text-xl">
@@ -39,7 +39,7 @@ const Sidebar = () => {
               className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
             />
             <span
-              className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-white rounded-full duration-300 ease-in-out 
+              className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-700 rounded-full duration-300 ease-in-out 
             peer-checked:bg-blue-600 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300
             peer-checked:after:translate-x-4 group-hover:after:translate-x-1"
             ></span>
@@ -52,7 +52,7 @@ const Sidebar = () => {
               className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
             />
             <span
-              className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-white rounded-full duration-300 ease-in-out 
+              className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-700 rounded-full duration-300 ease-in-out 
             peer-checked:bg-blue-600 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300
             peer-checked:after:translate-x-4 group-hover:after:translate-x-1"
             ></span>
@@ -65,7 +65,7 @@ const Sidebar = () => {
               className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
             />
             <span
-              className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-white rounded-full duration-300 ease-in-out 
+              className="w-11 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-700 rounded-full duration-300 ease-in-out 
             peer-checked:bg-blue-600 after:w-4 after:h-4 after:bg-white after:rounded-full after:shadow-md after:duration-300
             peer-checked:after:translate-x-4 group-hover:after:translate-x-1"
             ></span>
